172f807ced91105c7c782f2672b7876a4d6e4dfe
mahidamansoor211 <mahidamansoor211@gmail.com>
2026-01-15 23:59:01 +0500

    Add responsiveness to some features

--- src/Components/AppNav.jsx
+++ src/Components/AppNav.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={styles.nav}>
       <div className={styles.logo}>QuizDOM</div>
-      <ul className={styles.navLinks}>
+
+      {/* Hamburger Icon */}
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Links - Conditionally add the 'active' class */}
+      <ul className={`${styles.navLinks} ${isOpen ? styles.active : ""}`}>
         <li>
-          <NavLink to="/">About Us</NavLink>
+          <NavLink to="/" onClick={() => setIsOpen(false)}>
+            About Us
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/get-started">Get Started</NavLink>
+          <NavLink to="/get-started" onClick={() => setIsOpen(false)}>
+            Get Started
+          </NavLink>
         </li>
       </ul>
     </nav>
